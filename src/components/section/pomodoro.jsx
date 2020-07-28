0d2f48cad6e5c2d91f2561f1708c6d9723d25d65
Angelo T. Pavilando <pavilandoangelo@gmail.com>
2020-07-28 16:44:26 +0800

    update pomodoro

--- src/components/section/pomodoro.jsx
+++ src/components/section/pomodoro.jsx
@@ -6,6 +6,7 @@ export default function Pomodoro(props) {
     time: 1500,
     minutes: 0,
     seconds: 0,
+    started: false,
   });
 
   // useEffect(() => {
@@ -16,13 +17,50 @@ export default function Pomodoro(props) {
   //   console.log("seconds >>> ", seconds)
   // }, [])
 
-  useEffect(() => {
-    // state.time > 0 &&
-    //   setTimeout(() => setState({ ...state, time: state.time - 1 }), 1000);
+  const handleOnStart = () => {
+    return setState(state => ({
+      ...state,
+      started: true,
+    }));
+  };
+
+  const handleOnStop = () => {
+    return setState(state => ({
+      ...state,
+      started: false,
+    }));
+  };
+
+  const handleOnReset = () => {
+    return setState(state => ({
+      ...state,
+      started: false,
+      time: 1500,
+      minutes: Math.floor(1500 / 60),
+      seconds: 1500 % 60,
+    }));
+  };
+
+  const displayTime = () => {
     const minutes = Math.floor(state.time / 60);
     const seconds = state.time % 60;
-    setState({ ...state, minutes: minutes, seconds: seconds });
-  }, [state.time]);
+    setState(state => ({ ...state, minutes: minutes, seconds: seconds }));
+  };
+
+  useEffect(() => {
+    displayTime();
+  }, []);
+
+  useEffect(() => {
+    if (state.started) {
+      state.time > 0 &&
+        setTimeout(
+          () => setState(state => ({ ...state, time: state.time - 1 })),
+          1000
+        );
+      displayTime();
+    }
+  }, [state.time, state.started]);
 
   return (
     <>
@@ -42,18 +80,31 @@ export default function Pomodoro(props) {
             block
             className="btn-controls"
             style={{ textAlign: "center" }}
+            onClick={handleOnStart}
           >
             Start
           </Button>
         </Col>
         <Col xs={12} md={4} lg={2}>
-          <Button variant="danger" size="lg" block className="btn-controls">
+          <Button
+            variant="danger"
+            size="lg"
+            block
+            className="btn-controls"
+            onClick={handleOnStop}
+          >
             Stop
           </Button>
         </Col>
         <Col xs={12} md={4} lg={2}>
-          <Button variant="light" size="lg" block className="btn-controls">
-            Stop
+          <Button
+            variant="light"
+            size="lg"
+            block
+            className="btn-controls"
+            onClick={handleOnReset}
+          >
+            Reset
           </Button>
         </Col>
       </Row>
